5d3520a2ac0a0d08094093adf6b78cf6c05d0ae9
Mai Nguyen <chimai.dk@gmail.com>
2021-08-02 10:24:14 +1000

    changed form type

--- src/components/views/CalculatorForm.js
+++ src/components/views/CalculatorForm.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import InfoBox from '../InfoBox';
 import FormContent from '../FormContent';
 import Detail from '../Detail';
-import SubmitButton from '../SubmitButton'
+
 import TaxFormula from '../../helpers/TaxFormula';
 import TaxBrackets from '../../helpers/TaxBrackets';
 import currencyFormatter from '../../helpers/CurrencyFormatter';
@@ -18,107 +18,81 @@ export default function CalculatorForm() {
   let [taxDetails, setTaxDetails] = useState(TaxFormula['fy21']);
   let [totalTax, setTotalTax] = useState(0);
   // state of showing Form screen
-  let [isFormOn, setIsFromOn] = useState(true);
 
-  function breakdown () {
-    return taxDetails.map((bracket, index) => 
-      <Detail from={bracket.from} to={bracket.to} tax={bracket.tax} key={index}/>
-    )
-  };
-
-  // function _onChangeCountry(value) {
-  //   setCountry(value);
-  // }
-
-  // function _onChangeYear(value) {
-  //   setYear(value);
-  // }
-  // function _onChangeIncome(value) {
-  //   setIncome(value);
-  // }
-
-  function _onSubmit(e) {
-    e.preventDefault();
-    setIsFromOn(false);
+  function breakdown() {
+    if (taxDetails) {
+      return taxDetails.map((bracket, index) => (
+        <Detail
+          from={bracket.from}
+          to={bracket.to}
+          tax={bracket.tax}
+          key={index}
+        />
+      ));
+    }
   }
 
-  function redirect() {
-    setIsFromOn(true);
+  function _onChangeCountry(value) {
+    setCountry(value);
+  }
+
+  function _onChangeYear(value) {
+    setYear(value);
+  }
+  function _onChangeIncome(value) {
+    setIncome(value);
   }
 
   function countTotalTax(taxObj) {
     let total = 0;
     taxObj.forEach(bracket => {
       total += bracket.tax;
-    })
-    return total; 
+    });
+    return total;
   }
 
   useEffect(() => {
     // get Tax Object by country, year and income
-    const taxObj = TaxFormula.calculator(TaxBrackets[country][year], income)
+    const taxObj = TaxFormula.calculator(TaxBrackets[country][year], income);
     setTaxDetails(taxObj);
     setTotalTax(countTotalTax(taxObj));
-  }, [country, year, income])
+  }, [country, year, income]);
 
-  if (isFormOn) {
-    return (
+  return (
+    <div>
       <div className="form">
         <div className="background background-padding">
           <h1>Tax-o-tron</h1>
           <p>The free and simple online tax calculator.</p>
-          <div className="planetoid"></div>
-          <div className="moon"></div>
+          <div className="planetoid" />
+          <div className="moon" />
         </div>
         <div className="calculator">
-          <form onSubmit={_onSubmit}>
-            <h2>Calculate your tax</h2>
-            <InfoBox text="Fields marked with * are mandatory"/>
+          <h2>Calculate your tax</h2>
+          <InfoBox text="Fields marked with * are mandatory" />
 
-            <FormContent 
-              setCountry={setCountry}
-              setYear={setYear}
-              setIncome={setIncome}
-              country={country}
-              year={year}
-              income={income}
-              disabled={false}
-            />
-        
-            <SubmitButton text="Calculate" />
-          </form>
+          <FormContent
+            _onChangeCountry={_onChangeCountry}
+            _onChangeYear={_onChangeYear}
+            _onChangeIncome={_onChangeIncome}
+            country={country}
+            year={year}
+            income={income}
+            disabled={false}
+          />
         </div>
-
-      </div>
-    )
-  } else {
-    return (
         <div className="result">
-          <div className="calculator">
-            <h2>Your tax results</h2> 
-            <FormContent 
-              setCountry={setCountry}
-              setYear={setYear}
-              setIncome={setIncome}
-              country={country}
-              year={year}
-              income={income}
-              disabled={true}
-            />
-            <button onClick={redirect}>Go back to previous screen</button>
-          </div>
-          <div className="background">
-            <div className="planetoid"></div>
-            <div className="moon"></div>
-            <h3>Your estimated tax is:</h3>
-            <p className="result-total">{currencyFormatter(totalTax, 2)}</p>
-            <h3>Breakdown</h3>
-  
-            {breakdown()}
-          
-          </div>
+          {/* <div className="result-background"> */}
+          <div className="planetoid" />
+          <div className="moon" />
+          <h3>Your estimated tax is:</h3>
+          <p className="result-total">{currencyFormatter(totalTax, 2)}</p>
+          <h3>Breakdown</h3>
+
+          {breakdown()}
+          {/* </div> */}
         </div>
-    )
-  }
-  
+      </div>
+    </div>
+  );
 }
